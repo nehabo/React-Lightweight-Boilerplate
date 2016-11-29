2321982850faba46827ec2c5c7772c1562b225e3
nehabo <neha.bommareddy@gmail.com>
2016-11-29 22:56:59 +0530

    included css and bootstrap setup

--- app/index.js
+++ app/index.js
@@ -1,49 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const formatName = user => `${user.firstName} ${user.lastName}`;
-
-const user = {
-  firstName: 'Elif',
-  lastName: 'Denizer',
-};
-
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000,
-    );
-  }
-
-  componentWillUnMount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date(),
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>merhaba {formatName(user)}!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
+import Login from './login';
+import Timer from './timer';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div>
-      <Clock />
+      <Login />
+      <Timer />
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
     ],
   },
 // output for bundled files
